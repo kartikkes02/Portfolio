05878c7dffa07862afe61fb07582cba2f7856d63
Kartikeya Kesarwani <163706267+kartikkes02@users.noreply.github.com>
2025-11-22 18:44:21 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="./Kartikeya_Kesarwani.pdf" download="./Kartikeya_Kesarwani.pdf"
+                href="./_Kartikeya_Kesarwani.pdf" download="./_Kartikeya_Kesarwani.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
