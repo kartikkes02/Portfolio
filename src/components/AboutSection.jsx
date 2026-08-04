30f6a3d7b4d647c7b457a6915af31c982c7dc3f1
Kartikeya Kesarwani <163706267+kartikkes02@users.noreply.github.com>
2026-08-04 17:55:37 +0530

    Change link text from 'Download CV' to 'Download Resume'

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,7 @@ export const AboutSection = () => {
                 href="./Kartikeya_Kesarwani_Resume.pdf" download="./Kartikeya_Kesarwani_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
-                Download CV
+                Download Resume
               </a>
             </div>
           </div>
