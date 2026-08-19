047447c8ee652f8ea548283962285847a7c81a73
Kartikeya Kesarwani <163706267+kartikkes02@users.noreply.github.com>
2026-08-19 12:26:58 +0530

    Add AI Interview Platform project details
    Added a new project entry for the AI Interview Platform with relevant details.

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -41,6 +41,16 @@ const projects = [
     demoUrl: "https://shop-ease-self-omega.vercel.app/",
     githubUrl: "https://github.com/kartikkes02/FUTURE_FS_02",
   },
+  {
+    id: 3,
+    title: "AI Interview Platform",
+    description:
+      "A real-time AI interview agent that enables users to practice interviews interactively using voice.",
+    image: "/projects/ai_interview.png", 
+    tags: ["Nextjs", "Vapi Firebase","TypeScript"],
+    demoUrl: "https://ai-interview-platform-hazel.vercel.app/",
+    githubUrl: "https://github.com/kartikkes02/Ai-interview-platform",
+  },
   // {
   //   id: 5,
   //   title: "Simon Say Game",
