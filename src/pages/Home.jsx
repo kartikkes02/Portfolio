6a6bac718a13e4bf6101692e94bf9fbc373330ce
Kartikeya Kesarwani <163706267+kartikkes02@users.noreply.github.com>
2026-08-22 23:59:18 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Navbar } from "../components/Navbar";
-import { ThemeToggle } from "../components/ThemeToggle";
+import { ThemeToggle } from "../components/ThemeToggle"; 
 import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
